742112474f538da89a2e9796e8957467758106b2
idrissa.seydou-issaka <idrissa.seydou-issaka@mathinfo.i-univ-tlse2.fr>
2018-10-23 00:01:01 +0200

    maven projet

---
+++ src/test/java/tpmaven/mav/JXBusyLabelTestTest.java
@@ -0,0 +1,34 @@
+package tpmaven.mav;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JXBusyLabelTestTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testJXBusyLabelTest() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testActionPerformed() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		//fail("Not yet implemented");
+	}
+
+}
